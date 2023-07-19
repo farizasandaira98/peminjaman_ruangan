e79a283178764c0c3d4bdaf117fda25bbb12f575
farizasandaira98 <muhfariza98@gmail.com>
2023-07-19 07:54:45 +0700

    adding sql

---
+++ pinjam_ruang.sql
@@ -0,0 +1,321 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 18, 2023 at 02:27 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pinjam_ruang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_inventaris`
+--
+
+CREATE TABLE `data_inventaris` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_ruangan` bigint(20) UNSIGNED NOT NULL,
+  `nama_barang` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jumlah_barang` int(11) NOT NULL,
+  `kualitas_barang` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `data_inventaris`
+--
+
+INSERT INTO `data_inventaris` (`id`, `id_ruangan`, `nama_barang`, `jumlah_barang`, `kualitas_barang`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Termos', 1234, 1, '2023-07-15 14:08:22', '2023-07-15 14:21:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_peminjaman`
+--
+
+CREATE TABLE `data_peminjaman` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_peminjam` bigint(20) UNSIGNED NOT NULL,
+  `id_ruangan` bigint(20) UNSIGNED NOT NULL,
+  `keperluan_peminjaman` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `waktu_mulai_peminjaman` datetime NOT NULL,
+  `waktu_akhir_peminjaman` datetime NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `data_peminjaman`
+--
+
+INSERT INTO `data_peminjaman` (`id`, `id_peminjam`, `id_ruangan`, `keperluan_peminjaman`, `waktu_mulai_peminjaman`, `waktu_akhir_peminjaman`, `created_at`, `updated_at`) VALUES
+(8, 3, 1, 'test', '2023-07-18 09:56:00', '2023-07-18 10:11:00', '2023-07-17 11:57:07', '2023-07-17 11:57:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_ruangan`
+--
+
+CREATE TABLE `data_ruangan` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_ruangan` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kapasitas` int(11) NOT NULL,
+  `status_ruangan` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `data_ruangan`
+--
+
+INSERT INTO `data_ruangan` (`id`, `nama_ruangan`, `kapasitas`, `status_ruangan`, `created_at`, `updated_at`) VALUES
+(1, 'Ruangan 1', 2754, 2, '2023-07-13 16:57:34', '2023-07-17 11:57:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2023_07_08_081828_create_data_inventaris', 1),
+(6, '2023_07_08_082804_create_data_ruangan', 1),
+(7, '2023_07_08_083352_create_data_peminjaman', 1),
+(8, '2023_07_08_093805_add_foreign_key_to_data_inventaris', 1),
+(9, '2023_07_08_093829_add_foreign_key_to_data_peminjaman', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nip` bigint(20) NOT NULL,
+  `jabatan` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nomor_telepon` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` int(11) NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `nip`, `jabatan`, `nomor_telepon`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(3, 'Pengelola', 54321, 'Pejabat Ketua', '085397986721', 'pengelola@email.com', NULL, '$2y$10$SPK8I3KeVkA3uaVgH6.2Nu0dh1jOfcyvgdBzZZdoMa9VYDwJZNxL6', 1, NULL, '2023-07-09 05:04:56', NULL),
+(4, 'Fariza', 12345, 'Pejabat Dalam Lingkungan', '082345432348', 'email@test.com', NULL, '$2y$10$hL12MtXKZMIGjfcbxUb05O3ObA9FMTwk1d1vrGkRW7TX80v0UQrLi', 2, NULL, '2023-07-17 16:17:19', '2023-07-17 16:17:19');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_inventaris`
+--
+ALTER TABLE `data_inventaris`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `data_inventaris_id_ruangan_foreign` (`id_ruangan`);
+
+--
+-- Indexes for table `data_peminjaman`
+--
+ALTER TABLE `data_peminjaman`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `data_peminjaman_id_peminjam_foreign` (`id_peminjam`),
+  ADD KEY `data_peminjaman_id_ruangan_foreign` (`id_ruangan`);
+
+--
+-- Indexes for table `data_ruangan`
+--
+ALTER TABLE `data_ruangan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_inventaris`
+--
+ALTER TABLE `data_inventaris`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `data_peminjaman`
+--
+ALTER TABLE `data_peminjaman`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `data_ruangan`
+--
+ALTER TABLE `data_ruangan`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `data_inventaris`
+--
+ALTER TABLE `data_inventaris`
+  ADD CONSTRAINT `data_inventaris_id_ruangan_foreign` FOREIGN KEY (`id_ruangan`) REFERENCES `data_ruangan` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `data_peminjaman`
+--
+ALTER TABLE `data_peminjaman`
+  ADD CONSTRAINT `data_peminjaman_id_peminjam_foreign` FOREIGN KEY (`id_peminjam`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `data_peminjaman_id_ruangan_foreign` FOREIGN KEY (`id_ruangan`) REFERENCES `data_ruangan` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
